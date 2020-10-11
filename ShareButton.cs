ffb297d2ff27a259ba6e5e8633a800b909f57fa8
openG4NTZ <62043773+openG4NTZ@users.noreply.github.com>
2020-10-11 22:15:03 +0930

    Add files via upload

---
+++ ShareButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ShareButton : MonoBehaviour
+{
+	private string shareMessage;
+	public void ClickShareButton()
+    {
+		shareMessage = "Please let me know how this watch looks on my hand!!!";
+		StartCoroutine(TakeScreenshotAndShare());
+    }
+	private IEnumerator TakeScreenshotAndShare()
+	{
+		yield return new WaitForEndOfFrame();
+
+		Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+		ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		ss.Apply();
+
+		string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+		File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+		// To avoid memory leaks
+		Destroy(ss);
+
+		new NativeShare().AddFile(filePath)
+			.SetSubject("Augmented Reality Watch").SetText(shareMessage)
+			.SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+			.Share();
+	}
+}
---
+++ Uimanager.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+ 
+using UnityEngine.SceneManagement;
+
+public class Uimanager : MonoBehaviour
+{
+    public GameObject Watchpage;
+    public GameObject Catagory_Panel;
+    public GameObject Metal_Panel;
+    public GameObject Sport_Panel;
+    public GameObject Smart_Panel;
+    public GameObject Leather_Panel;
+    public GameObject Main_Panel;
+    public AudioSource asource;
+    public AudioClip aclip1;
+    public AudioClip aclip2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Watchpage.GetComponent<WatchesPages>().enabled = false;
+        Metal_Panel.SetActive(false);
+        Sport_Panel.SetActive(false);
+        Smart_Panel.SetActive(false);
+        Leather_Panel.SetActive(false);
+        Main_Panel.SetActive(true);
+        asource.PlayOneShot(aclip1);
+    }
+
+
+    public void back_Smart()
+    {
+        Smart_Panel.SetActive(false);
+        Catagory_Panel.SetActive(true);
+        bTN_SOUND();
+    }
+    public void back_Sport()
+    {
+        Sport_Panel.SetActive(false);
+        Catagory_Panel.SetActive(true);
+        bTN_SOUND();
+    }
+    public void back_Leather()
+    {
+        Catagory_Panel.SetActive(true);
+        Leather_Panel.SetActive(false);
+        bTN_SOUND();
+    }
+    public void back_Metal()
+    {
+        Catagory_Panel.SetActive(true);
+        Metal_Panel.SetActive(false);
+        bTN_SOUND();
+    }
+
+    public void Open_Smart()
+    {
+        Catagory_Panel.SetActive(false);
+        Smart_Panel.SetActive(true);
+        bTN_SOUND();
+    }
+    public void Open_Sport()
+    {
+        Sport_Panel.SetActive(true);
+        Catagory_Panel.SetActive(false);
+        bTN_SOUND();
+    }
+    public void Open_Leather()
+    {
+        Leather_Panel.SetActive(true);
+        Catagory_Panel.SetActive(false);
+        bTN_SOUND();
+    }
+    public void Open_Metal()
+    {
+        Metal_Panel.SetActive(true);
+        Catagory_Panel.SetActive(false);
+        bTN_SOUND();
+    }
+
+    public void Open_Catagory()
+    {
+        Catagory_Panel.SetActive(true);
+        Main_Panel.SetActive(false);
+        bTN_SOUND();
+    }
+    public void Open_mainpage()
+    {
+        Catagory_Panel.SetActive(false);
+        Main_Panel.SetActive(true);
+        bTN_SOUND();
+    }
+    public void select_Leather_watch1(int num)
+    {
+        if (num == 0)
+        {
+            PlayerPrefs.SetInt("Leather", num);
+            SceneManager.LoadScene("Leather_watch_AR");
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+            bTN_SOUND();
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+        }
+        if (num == 1)
+        {
+            PlayerPrefs.SetInt("Leather", num);
+            SceneManager.LoadScene("Leather_watch_AR");
+           
+           
+            bTN_SOUND();
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+        }
+        if (num == 2)
+        {
+            PlayerPrefs.SetInt("Leather", num);
+            SceneManager.LoadScene("Leather_watch_AR");
+            bTN_SOUND();
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+        }
+        if (num == 3)
+        {
+            PlayerPrefs.SetInt("Leather", num);
+            SceneManager.LoadScene("Leather_watch_AR");
+            bTN_SOUND();
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+        }
+        //======================================================================
+       
+        //======================================
+       
+        //==============================================================
+     
+    }
+    public void select_Smart_watch2(int num)
+    {
+        if (num == 0)
+        {
+            PlayerPrefs.SetInt("Smart", num);
+            SceneManager.LoadScene("Smart_watch_AR");
+            bTN_SOUND();
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+        }
+        if (num == 1)
+        {
+            PlayerPrefs.SetInt("Smart", num);
+            SceneManager.LoadScene("Smart_watch_AR");
+            bTN_SOUND();
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+        }
+        if (num == 2)
+        {
+            PlayerPrefs.SetInt("Smart", num);
+            SceneManager.LoadScene("Smart_watch_AR");
+            bTN_SOUND();
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+        }
+        if (num == 3)
+        {
+            PlayerPrefs.SetInt("Smart", num);
+            SceneManager.LoadScene("Smart_watch_AR");
+            bTN_SOUND();
+            Watchpage.GetComponent<WatchesPages>().enabled = true;
+        }
+
+    }
+    public void select_Metal_watch3(int num)
+    {
+
+        if (num == 0)
+        {
+            PlayerPrefs.SetInt("Metal", num);
+            SceneManager.LoadScene("Metal_watch_AR");
+            bTN_SOUND();
+        }
+        if (num == 1)
+        {
+            PlayerPrefs.SetInt("Metal", num);
+            SceneManager.LoadScene("Metal_watch_AR");
+            bTN_SOUND();
+        }
+        if (num == 2)
+        {
+            PlayerPrefs.SetInt("Metal", num);
+            SceneManager.LoadScene("Metal_watch_AR");
+            bTN_SOUND();
+        }
+        if (num == 3)
+        {
+            PlayerPrefs.SetInt("Metal", num);
+            SceneManager.LoadScene("Metal_watch_AR");
+            bTN_SOUND();
+        }
+    }
+    public void select_Sport_watch4(int num)
+    {
+        if (num == 0)
+        {
+            PlayerPrefs.SetInt("Sport", num);
+            SceneManager.LoadScene("Sport_watch_AR");
+            bTN_SOUND();
+        }
+        if (num <= 1)
+        {
+            PlayerPrefs.SetInt("Sport", num);
+            SceneManager.LoadScene("Sport_watch_AR");
+            bTN_SOUND();
+        }
+        if (num <= 2)
+        {
+            PlayerPrefs.SetInt("Sport", num);
+            SceneManager.LoadScene("Sport_watch_AR");
+            bTN_SOUND();
+        }
+        if (num <= 3)
+        {
+            PlayerPrefs.SetInt("Sport", num);
+            SceneManager.LoadScene("Sport_watch_AR");
+            bTN_SOUND();
+        }
+
+    }
+    public void bTN_SOUND()
+    {
+        asource.Stop();
+        asource.PlayOneShot(aclip2);
+    }
+
+
+    public void WatchesPages_pagesON()
+    {
+
+        Watchpage.GetComponent<WatchesPages>().enabled = true;
+    }
+}
